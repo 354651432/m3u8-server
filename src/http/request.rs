498a10921f48e6e8342598f42b5cac7ef0cd1cf2
aniki <aniki@dr.h>
2022-05-09 11:41:57 +0800

    rebuild request

--- src/http/request.rs
+++ src/http/request.rs
@@ -8,117 +8,83 @@ use std::{
 
 use serde::Deserialize;
 
+use super::{getkey_ignorecase, header_tostr, parse_headers, parse_reqline, Headers, ReqLine};
+
 #[cfg(test)]
 #[path = "request_test.rs"]
 mod tests;
 
 #[derive(Debug)]
 pub struct Request {
-    pub version: String,
-    pub path: String,
-    pub method: String,
-    pub headers: HashMap<String, String>,
+    pub req: ReqLine,
+    pub headers: Headers,
     pub body: Vec<u8>,
-
-    // 反序列化用的字段
-    body_str: String,
 }
 
 impl Request {
-    pub fn new() -> Self {
-        Self {
-            version: String::from("HTTP/1.1"),
-            path: String::from("/"),
-            method: String::from("GET"),
-            headers: Default::default(),
-            body: Default::default(),
-            body_str: Default::default(),
-        }
+    pub fn new(req: ReqLine, headers: Headers, body: Vec<u8>) -> Self {
+        Self { req, headers, body }
     }
 
     // 这个时候 谁拥有stream是有由主调方决定 可以传值或者引用
     pub fn read_from_stream(stream: impl Read) -> Option<Request> {
         let mut reader = BufReader::new(stream);
 
-        let mut buf = String::new();
+        // 去掉开头的空行
+        let mut line = String::new();
 
-        while buf.len() <= 0 {
-            reader.read_line(&mut buf).ok()?;
+        let mut cnt = 0;
+        while line.len() <= 0 && cnt < 100 {
+            reader.read_line(&mut line).ok()?;
+            cnt += 1
         }
 
-        let (method, path, version) = Self::parse_status_line(buf)?;
+        let req = parse_reqline(&line)?;
 
-        let mut header_str = String::new();
+        let mut lines = Vec::new();
         let mut cnt = 0;
         while cnt < 1024 {
             let mut buf = String::new();
             reader.read_line(&mut buf).ok()?;
-            header_str += &buf;
-            header_str += "\r\n";
-            if header_str.ends_with("\r\n\r\n") {
+            if buf.is_empty() {
                 break;
             }
+            lines.push(buf);
             cnt += 1;
         }
-        let mut headers: HashMap<String, String> = HashMap::new();
-        for line in header_str.split("\r\n") {
-            let mut it = line.split(":");
-            let key = it.next();
-            if key.is_none() {
-                continue;
-            }
 
-            let value = it.next();
-            if value.is_none() {
-                continue;
-            }
-            headers.insert(key?.trim().to_owned(), value?.trim().to_owned());
-        }
+        let headers = parse_headers(&lines);
 
         let mut body = Vec::new();
-        if let Some(content_length) = headers.get("Content-Length") {
-            let content_length: Result<usize, _> = content_length.parse();
-            if let Ok(content_length) = content_length {
-                body = Vec::with_capacity(content_length);
-                reader.read(&mut body).ok()?;
+        let content_length = getkey_ignorecase("content-length", &headers);
+
+        if let Some(content_length) = content_length {
+            if let Ok(capacity) = content_length.parse() {
+                body = Vec::with_capacity(capacity);
+                unsafe {
+                    body.set_len(capacity);
+                }
+                reader.read(&mut body);
             }
+        } else if (req.method == "POST" || req.method == "PUT") {
+            reader.read_to_end(&mut body);
         }
 
-        Some(Request {
-            method,
-            path,
-            version,
-            headers,
-            body,
-
-            body_str: Default::default(),
-        })
+        Some(Request { req, headers, body })
     }
 
     pub fn body_str(&self) -> String {
         String::from_utf8_lossy(&self.body).to_string()
     }
 
-    // pub fn body_str_ref(&self) -> &str {
-    //     &String::from_utf8_lossy(&self.body).to_string()
+    // TODO
+    // pub fn json<'de, T>(&'de mut self) -> Result<T, serde_json::Error>
+    // where
+    //     T: Deserialize<'de>,
+    // {
+    //     let rdr = BufReader::new(self.body_str().as_bytes());
+    //     serde_json::from_reader(rdr)
     // }
-
-    fn parse_status_line(buf: String) -> Option<(String, String, String)> {
-        let mut it = buf.split_whitespace();
-        let method = it.next()?.to_uppercase();
-        let path = it.next()?;
-        let version = it.next()?;
-        Some((method, path.to_owned(), version.to_owned()))
-    }
-
-    pub fn json<'b, T>(&'b mut self) -> Option<T>
-    where
-        T: Deserialize<'b>,
-    {
-        self.body_str = self.body_str();
-        let ret: T = serde_json::from_str(&self.body_str).ok()?;
-        Some(ret)
-    }
 
     pub fn header(&mut self, key: &str, value: &str) -> Option<String> {
         self.headers.insert(key.to_owned(), value.to_owned())
@@ -127,14 +93,9 @@ impl Request {
 
 impl Display for Request {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} {} {}\r\n", self.method, self.path, self.version)?;
-
-        let mut header_str = String::new();
-        for (key, value) in &self.headers {
-            write!(f, "{}: {}\r\n", key, value)?;
-        }
+        write!(f, "{}\r\n", self.req)?;
+        write!(f, "{}\r\n", header_tostr(&self.headers))?;
         write!(f, "\r\n")?;
-
         write!(f, "{}", self.body_str());
         Ok(())
     }
--- src/http/request_test.rs
+++ src/http/request_test.rs
@@ -12,9 +12,7 @@ fn test_parse_req() {
 
     let req = Request::read_from_stream(stream).unwrap();
     // assert_eq!(req.body, None);
-    assert_eq!(req.version, "HTTP/1.1");
-    assert_eq!(req.method, "GET");
-    assert_eq!(req.path, "/api");
+    assert_eq!(req.req.to_string(), "GET /api HTTP/1.1");
     assert_eq!(req.headers["Host"], "ducl.cc");
     assert_eq!(req.headers["Accept"], "application/json");
 }
