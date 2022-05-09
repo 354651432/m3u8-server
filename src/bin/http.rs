4a9f2cdd0064e3bfa3ec4412892f6c7f231cb262
aniki <aniki@dr.h>
2022-05-09 13:04:30 +0800

    add thread

--- src/bin/http.rs
+++ src/bin/http.rs
@@ -1,7 +1,11 @@
+use std::thread;
+
 use work::http::http_listener::HttpListener;
 
 fn main() {
     for mut ctx in HttpListener::bind("127.0.0.1:8080") {
-        ctx.res.body("<h1>it works!");
+        thread::spawn(move || {
+            ctx.res.body("<h1>it works!");
+        });
     }
 }
