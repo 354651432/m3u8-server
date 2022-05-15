5296160b7ca2770a4c0e6b2b422b230fee97b65b
aniki <aniki@dr.h>
2022-05-15 12:04:14 +0800

    rebuild

--- src/http/http_client.rs
+++ src/http/http_client.rs
@@ -40,7 +40,7 @@ impl HttpClient {
     }
 
     pub fn proxy(&mut self, proxy: &str) -> &Self {
-        // self.proxy = proxy.to_lowercase().replace("socks5://", "");
+        self.proxy = proxy.to_string();
         self
     }
 
@@ -56,14 +56,14 @@ impl HttpClient {
         )
     }
 
-    fn build_base_stream(&self, url: &Url) -> Result<HttpWriter, String> {
+    fn build_base_stream(&self, url: &Url) -> Result<StreamWapper, String> {
         if !self.proxy.trim().is_empty() {
             // TODO 增加http
             let proxy = self.proxy.to_lowercase().replace("socks5://", "");
             match Socks5Stream::connect(proxy, url.to_host().as_str()) {
                 Ok(stream) => {
                     // cannot set timeout for Socks5Stream
-                    return Ok(HttpWriter::Scoks5(stream));
+                    return Ok(StreamWapper::from_stream(stream));
                 }
                 Err(err) => return Err(err.to_string()),
             }
@@ -73,40 +73,28 @@ impl HttpClient {
             Ok(stream) => {
                 stream.set_read_timeout(Some(READ_TIMEOUT));
                 stream.set_write_timeout(Some(WRITE_TIMEOUT));
-                Ok(HttpWriter::TcpStream(stream))
+                Ok(StreamWapper::from_stream(stream))
             }
             Err(err) => Err(err.to_string()),
         }
     }
 
-    fn build_https_stream(
-        &self,
-        url: &Url,
-        stream: HttpWriter,
-    ) -> Result<Box<dyn IReadWriter>, String> {
+    fn build_https_stream(&self, url: &Url, stream: StreamWapper) -> Result<StreamWapper, String> {
         let connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
-        match stream {
-            HttpWriter::TcpStream(stream) => {
-                let mut stream = match connector.connect(&url.host, stream) {
-                    Ok(stream) => stream,
-                    Err(err) => return Err(err.to_string()),
-                };
-                Ok(Box::new(stream))
+        let mut stream = match connector.connect(&url.host, stream) {
+            Ok(stream) => stream,
+            Err(err) => {
+                100;
+                return Err("build https stream failed".to_string());
             }
-            HttpWriter::Scoks5(stream) => {
-                let mut stream = match connector.connect(&url.host, stream) {
-                    Ok(stream) => stream,
-                    Err(err) => return Err(err.to_string()),
-                };
-                Ok(Box::new(stream))
-            }
-        }
+        };
+        Ok(StreamWapper::from_stream(stream))
     }
 
-    fn build_stream(&self, url: Url) -> Result<Box<dyn IReadWriter>, String> {
+    fn build_stream(&self, url: Url) -> Result<StreamWapper, String> {
         let stream = self.build_base_stream(&url);
         if url.proto.to_lowercase() == "http" {
-            return Ok(Box::new(stream?));
+            return Ok(stream?);
         }
 
         self.build_https_stream(&url, stream?)
@@ -130,11 +118,11 @@ impl HttpClient {
 
         // println!("{:#?}", req.headers);
         let buf = req.to_string();
-        if let Err(err) = stream.write(buf.as_bytes()) {
+        if let Err(err) = Write::write(&mut stream, buf.as_bytes()) {
             return Err(err.to_string());
         }
 
-        match Response::from_stream(LocalRead::new(stream)) {
+        match Response::from_stream(stream) {
             Some(res) => {
                 let code = res.res.code;
                 if (200..=300).contains(&code) {
